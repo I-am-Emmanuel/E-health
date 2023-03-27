e57cd1425bdaa6a675a0a64c16ad0d4bd1892aa6
Isah Jacob <isahjacob0@gmail.com>
2023-03-27 14:55:45 +0100

    created the doctor model

--- services/doctor_service/models.py
+++ services/doctor_service/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Doctor(models.Model):
+    SPECIALTY_CHOICES = (
+        ('epidemiologist', 'Epidemiologist'),
+        ('general practitioner', 'General practitioner'),
+        ('pediatrician', 'Pediatrician'),
+        ('dentist', 'Dentist'),
+        ('surgeon', 'Surgeon'),
+        ('dermatologist', 'Dermatologist'),
+        ('plastic surgeon', ' Plastic Surgeon'),
+        ('psychiatrist', 'Psychiatrist'),
+    )
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=16)
+    specialty = models.CharField(max_length=20, choices=SPECIALTY_CHOICES, default='psychiatrist')
+    available_days = models.DateTimeField()
+
+    class Meta:
+        ordering = ('available_days',)
+
+    def __str__(self):
+        return f'{self.first_name} - {self.last_name} {self.specialty} {self.available_days}'
