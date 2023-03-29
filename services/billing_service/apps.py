bd507343421a35e063a68e4ed2519bef2339e316
Isah Jacob <isahjacob0@gmail.com>
2023-03-29 12:04:15 +0100

    apps file for billing service

--- services/billing_service/apps.py
+++ services/billing_service/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BillingServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'billing_service'
+    name = 'services.billing_service'
