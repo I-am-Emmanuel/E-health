35260697d29d743aa7b02dca73746c08325a3bef
Isah Jacob <isahjacob0@gmail.com>
2023-03-29 12:04:41 +0100

    apps file for patient service

--- services/patient_service/apps.py
+++ services/patient_service/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class PatientServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'patient_service'
+    name = 'services.patient_service'
+
